c290effe04a75b99c3f880cd020e25a2de36ea3a
Obavictor03 <obarovictor03@gmail.com>
2026-01-11 14:59:08 +0100

    interacting with the admin panel

--- LibraryProject/bookshelf/admin.py
+++ LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+
+class Bookadmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'publication_year')
+    list_filter = ('title', 'publication_year')
+    search_fields = ('title', 'author')
+    ordering = ('title')
+    list_per_page = 20

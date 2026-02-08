e0dc867869c4a4268a6f3c749cbd11978fbde3b4
Obavictor03 <obarovictor03@gmail.com>
2026-02-08 15:03:30 +0100

    custom views and generic views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
+# Create your views here.
 
 
 class BookListView(generics.ListAPIView):

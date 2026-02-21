71d75781a65b5cb340659c9af1d1dbeb3ebebc64
Obavictor03 <obarovictor03@gmail.com>
2026-02-21 01:41:25 +0100

    using django built-in method to create user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,7 @@ from .models import CustomUser
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 
 User = get_user_model()
@@ -14,10 +15,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
-        user.set_password(password)
-        user.save()
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
     
 class LoginSerializer(serializers.Serializer):
